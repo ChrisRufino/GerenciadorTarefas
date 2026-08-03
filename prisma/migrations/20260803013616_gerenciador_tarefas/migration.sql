d143f8ec8d3f92bfa014473785139b86f738cbb7
ChriRufino <christisael@gmail.com>
2026-08-02 23:16:56 -0300

    postFinish

---
+++ prisma/migrations/20260803013616_gerenciador_tarefas/migration.sql
@@ -0,0 +1,89 @@
+-- CreateEnum
+CREATE TYPE "userRole" AS ENUM ('administrator', 'member');
+
+-- CreateEnum
+CREATE TYPE "status" AS ENUM ('pending', 'making', 'completed');
+
+-- CreateEnum
+CREATE TYPE "priority" AS ENUM ('high', 'low', 'average');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(150) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "userRole" NOT NULL DEFAULT 'member',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teams" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "teams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "team_members" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "team_id" INTEGER NOT NULL,
+
+    CONSTRAINT "team_members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(200) NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "status" NOT NULL DEFAULT 'pending',
+    "priority" "priority" NOT NULL DEFAULT 'average',
+    "assigned_to" INTEGER NOT NULL,
+    "team_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks_history" (
+    "id" SERIAL NOT NULL,
+    "task_id" INTEGER NOT NULL,
+    "changed_by" INTEGER NOT NULL,
+    "old_status" "status" NOT NULL,
+    "new_status" "status" NOT NULL,
+    "changed_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "tasks_history_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "team_members" ADD CONSTRAINT "team_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_members" ADD CONSTRAINT "team_members_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_assigned_to_fkey" FOREIGN KEY ("assigned_to") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks_history" ADD CONSTRAINT "tasks_history_task_id_fkey" FOREIGN KEY ("task_id") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks_history" ADD CONSTRAINT "tasks_history_changed_by_fkey" FOREIGN KEY ("changed_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,10 +1,42 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { hash } from "bcrypt";
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
   async create(request: Request, response: Response) {
-    return response.status(201).json({
-      message: "user created sucessfully",
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
     });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const userWithSameEmail = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError("Email already in use", 409);
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.users.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.json(userWithoutPassword);
   }
 }
 

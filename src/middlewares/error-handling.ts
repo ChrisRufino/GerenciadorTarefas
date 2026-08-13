cf0e78326b2b7421a7958c5cae1a152893ef62f1
ChriRufino <christisael@gmail.com>
2026-08-12 23:18:50 -0300

    gerenciador

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -4,13 +4,15 @@ import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 
 export function errorHandling(
-  error: any,
+  error: Error,
   request: Request,
   response: Response,
   next: NextFunction,
 ) {
   if (error instanceof AppError) {
-    return response.status(error.statusCode).json({ message: error.message });
+    return response.status(error.statusCode).json({
+      message: error.message,
+    });
   }
   if (error instanceof ZodError) {
     return response.status(400).json({

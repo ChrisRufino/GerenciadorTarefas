5192393bece73e0e75c792f84e6cc6ed7387c30b
ChriRufino <christisael@gmail.com>
2026-08-24 14:22:13 -0300

    remove log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { sessionsRoutes } from "./sessions-routes";
 import { tasksHistoryRoutes } from "./tasks-history-routes";
-import { tasksLogsRoutes } from "./tasks-logs-routes";
 import { tasksRoutes } from "./tasks-routes";
 import { listMembers, membersRoutes } from "./team-members-routes";
 import { teamsRoutes } from "./teams-routes";
@@ -15,7 +14,6 @@ routes.use("/tasks", tasksRoutes);
 routes.use("/teams", teamsRoutes);
 routes.use("/members", membersRoutes);
 routes.use("/membersList", listMembers);
-routes.use("/tasks-log", tasksLogsRoutes);
 routes.use("/tasks-history", tasksHistoryRoutes);
 
 export { routes };
